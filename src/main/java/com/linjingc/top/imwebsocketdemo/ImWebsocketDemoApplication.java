f6ce7a43fdea180157ba9d16011865f6a7e30572
chenxiaocehng <583188551@qq.com>
2019-11-07 15:00:17 +0800

    Initial commit

---
+++ src/main/java/com/linjingc/top/imwebsocketdemo/ImWebsocketDemoApplication.java
@@ -0,0 +1,13 @@
+package com.linjingc.top.imwebsocketdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImWebsocketDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImWebsocketDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linjingc/top/imwebsocketdemo/ImWebsocketDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linjingc.top.imwebsocketdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImWebsocketDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
